8508a9eaa9da59435e1a96be14c3ec0992cbb644
trungduc1122 <100517525+trungduc1122@users.noreply.github.com>
2022-06-30 07:56:18 +0700

    create Member DAO, IMemberRepository, MemberRepository update Member DAO checkLogin and getmembers

---
+++ DataAccess/MemberDAO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObject;
+
+namespace DataAccess
+{
+    public class MemberDAO
+    {
+        Member checkLogin(string email,string password)
+        {
+            Member member = null;
+            using var db = new FStoreDBAssignmentContext();
+            member =(Member)db.Member.Where(x => x.Email == email && x.Password == password);
+            return member;
+        }
+
+        List<Member> GetMembers()
+        {
+            List<Member> list = null;
+            using var db = new FStoreDBAssignmentContext();
+            list = db.Member.ToList<Member>();
+            return list;
+        }
+
+    }
+}
