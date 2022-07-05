250dddd236f79d8519c1e0e00c0c6204c4f37ea9
trungduc1122 <100517525+trungduc1122@users.noreply.github.com>
2022-07-05 21:47:22 +0700

    working on frm member loadmembers not yet complete

---
+++ SalesWinApp/frmMemberDetail.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace SalesWinApp
+{
+    partial class frmMemberDetail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtCountry = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(154, 275);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 43;
+            this.button3.Text = "CANCEL";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(54, 275);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 42;
+            this.button2.Text = "SAVE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtCountry
+            // 
+            this.txtCountry.Location = new System.Drawing.Point(133, 227);
+            this.txtCountry.Name = "txtCountry";
+            this.txtCountry.Size = new System.Drawing.Size(173, 27);
+            this.txtCountry.TabIndex = 41;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(133, 184);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(173, 27);
+            this.txtCity.TabIndex = 40;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(133, 140);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(173, 27);
+            this.txtPassword.TabIndex = 39;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(133, 94);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(173, 27);
+            this.txtEmail.TabIndex = 38;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(133, 53);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(173, 27);
+            this.txtName.TabIndex = 37;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(133, 6);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(173, 27);
+            this.txtID.TabIndex = 36;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 230);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 20);
+            this.label6.TabIndex = 35;
+            this.label6.Text = "COUNTRY";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 184);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 20);
+            this.label5.TabIndex = 34;
+            this.label5.Text = "CITY";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 20);
+            this.label4.TabIndex = 33;
+            this.label4.Text = "PASSWORD";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 32;
+            this.label3.Text = "EMAIL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 20);
+            this.label2.TabIndex = 31;
+            this.label2.Text = "COMPANY NAME";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 20);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "MEMBER ID";
+            // 
+            // frmMemberDetail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 313);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtCountry);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMemberDetail";
+            this.Text = "frmMemberDetail";
+            this.Load += new System.EventHandler(this.frmMemberDetail_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button3;
+        private Button button2;
+        private TextBox txtCountry;
+        private TextBox txtCity;
+        private TextBox txtPassword;
+        private TextBox txtEmail;
+        private TextBox txtName;
+        private TextBox txtID;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+    }
+}
---
+++ SalesWinApp/frmMemberDetail.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataAccess.Repository;
+using BusinessObject;
+
+namespace SalesWinApp
+{
+    public partial class frmMemberDetail : Form
+    {
+        public frmMemberDetail()
+        {
+            InitializeComponent();
+        }
+        public IMemberRepository memberRepository { get; set; }
+        public bool InsertOrUpdate { get; set; }
+        public Member info;
+        private void button3_Click(object sender, EventArgs e) => Close();
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var member = new Member
+                {
+                    MemberId = int.Parse(txtID.Text),
+                    CompanyName=txtName.Text,
+                    Email = txtEmail.Text,
+                    Password=txtPassword.Text,
+                    City = txtCity.Text,
+                    Country = txtCountry.Text
+                };
+                if (InsertOrUpdate == false)
+                {
+                    memberRepository.UpdateMember(member);
+                }
+                else
+                {
+                    memberRepository.AddMember(member);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, InsertOrUpdate == false ? "add" : "update");
+            }
+        }
+
+        private void frmMemberDetail_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
