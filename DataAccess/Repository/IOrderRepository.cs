c5a726cb3d5f57f75fb725a5f952b7df8bccefbf
trungduc1122 <100517525+trungduc1122@users.noreply.github.com>
2022-07-11 17:49:20 +0700

    re update iorder order repo

---
+++ DataAccess/Repository/IOrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObject;
+
+namespace DataAccess.Repository
+{
+    public interface IOrderRepository
+    {
+        public List<Order> GetOrders();
+        public void AddOrder(Order order);
+        public void UpdateOrder(Order order);
+        public void DeleteOrder(Order order);
+    }
+}
---
+++ DataAccess/Repository/OrderRepository.cs
@@ -0,0 +1,20 @@
+﻿using BusinessObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class OrderRepository : IOrderRepository
+    {
+        public void AddOrder(Order order) => OrderDAO.Instance.AddOrder(order);
+
+        public void DeleteOrder(Order order) => OrderDAO.Instance.DeleteOrder(order);
+
+        public List<Order> GetOrders() => OrderDAO.Instance.GetOrders();
+
+        public void UpdateOrder(Order order) => OrderDAO.Instance.UpdateOrder(order);
+    }
+}
