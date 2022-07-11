a72636b9535bac01f774d50331a9efa8f0b5b353
trungduc1122 <100517525+trungduc1122@users.noreply.github.com>
2022-07-11 17:29:08 +0700

    Revert "working on frm member"
    This reverts commit 250dddd236f79d8519c1e0e00c0c6204c4f37ea9.

--- SalesWinApp/frmMember.Designer.cs
+++ SalesWinApp/frmMember.Designer.cs
@@ -49,137 +49,136 @@
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(615, 260);
+            this.textBox6.Location = new System.Drawing.Point(453, 110);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(173, 27);
+            this.textBox6.Size = new System.Drawing.Size(125, 27);
             this.textBox6.TabIndex = 31;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(615, 217);
+            this.textBox5.Location = new System.Drawing.Point(453, 66);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(173, 27);
+            this.textBox5.Size = new System.Drawing.Size(125, 27);
             this.textBox5.TabIndex = 30;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(615, 173);
+            this.textBox4.Location = new System.Drawing.Point(453, 25);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(173, 27);
+            this.textBox4.Size = new System.Drawing.Size(125, 27);
             this.textBox4.TabIndex = 29;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(615, 127);
+            this.textBox3.Location = new System.Drawing.Point(104, 110);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(173, 27);
+            this.textBox3.Size = new System.Drawing.Size(125, 27);
             this.textBox3.TabIndex = 28;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(615, 86);
+            this.textBox2.Location = new System.Drawing.Point(104, 66);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(173, 27);
+            this.textBox2.Size = new System.Drawing.Size(125, 27);
             this.textBox2.TabIndex = 27;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(615, 39);
+            this.textBox1.Location = new System.Drawing.Point(104, 28);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(173, 27);
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
             this.textBox1.TabIndex = 26;
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 20);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 237);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 29;
-            this.dataGridView1.Size = new System.Drawing.Size(476, 410);
+            this.dataGridView1.Size = new System.Drawing.Size(776, 188);
             this.dataGridView1.TabIndex = 25;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(694, 318);
+            this.button3.Location = new System.Drawing.Point(584, 162);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(94, 29);
             this.button3.TabIndex = 24;
-            this.button3.Text = "DELETE";
+            this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(594, 318);
+            this.button2.Location = new System.Drawing.Point(341, 162);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(94, 29);
             this.button2.TabIndex = 23;
-            this.button2.Text = "NEW";
+            this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(494, 318);
+            this.button1.Location = new System.Drawing.Point(92, 162);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(94, 29);
             this.button1.TabIndex = 22;
-            this.button1.Text = "LOAD";
+            this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(494, 263);
+            this.label6.Location = new System.Drawing.Point(397, 113);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(75, 20);
+            this.label6.Size = new System.Drawing.Size(50, 20);
             this.label6.TabIndex = 21;
-            this.label6.Text = "COUNTRY";
+            this.label6.Text = "label6";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(494, 217);
+            this.label5.Location = new System.Drawing.Point(397, 69);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(38, 20);
+            this.label5.Size = new System.Drawing.Size(50, 20);
             this.label5.TabIndex = 20;
-            this.label5.Text = "CITY";
+            this.label5.Text = "label5";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(494, 176);
+            this.label4.Location = new System.Drawing.Point(397, 28);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(87, 20);
+            this.label4.Size = new System.Drawing.Size(50, 20);
             this.label4.TabIndex = 19;
-            this.label4.Text = "PASSWORD";
+            this.label4.Text = "label4";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(494, 130);
+            this.label3.Location = new System.Drawing.Point(48, 113);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.Size = new System.Drawing.Size(50, 20);
             this.label3.TabIndex = 18;
-            this.label3.Text = "EMAIL";
+            this.label3.Text = "label3";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(494, 89);
+            this.label2.Location = new System.Drawing.Point(48, 69);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(124, 20);
+            this.label2.Size = new System.Drawing.Size(50, 20);
             this.label2.TabIndex = 17;
-            this.label2.Text = "COMPANY NAME";
+            this.label2.Text = "label2";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(494, 42);
+            this.label1.Location = new System.Drawing.Point(48, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(88, 20);
+            this.label1.Size = new System.Drawing.Size(50, 20);
             this.label1.TabIndex = 16;
-            this.label1.Text = "MEMBER ID";
+            this.label1.Text = "label1";
             // 
             // frmMember
             // 
--- SalesWinApp/frmMember.cs
+++ SalesWinApp/frmMember.cs
@@ -7,8 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using BusinessObject;
-using DataAccess.Repository;
 
 namespace SalesWinApp
 {
@@ -18,29 +16,5 @@ namespace SalesWinApp
         {
             InitializeComponent();
         }
-        BindingSource source;
-        IMemberRepository memberRepository1 = new MemberRepository();
-        public void Loadmembers()
-        {
-            var list = memberRepository1.GetMembers();
-            source = new BindingSource();
-            source.DataSource = list;
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            frmMemberDetail frmMemberDetail = new frmMemberDetail
-            {
-                Text = "Add member",
-                InsertOrUpdate = false,
-                memberRepository = memberRepository1
-            };
-            if(frmMemberDetail.ShowDialog() == DialogResult.OK)
-            {
-                Loadmembers();
-            }
-        }
-
-        
     }
 }
